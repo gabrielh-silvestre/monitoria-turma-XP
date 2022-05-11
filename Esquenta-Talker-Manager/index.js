cff0fcd71d36fd6e32b7432e65e9634df0a59e37
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-11 16:06:26 -0300

    feat(routes/task): create task route
    - use task route on index.js

--- Esquenta-Talker-Manager/index.js
+++ Esquenta-Talker-Manager/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { taskRoute } = require('./routes/task');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
@@ -10,6 +11,8 @@ app.get('/', (_req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use('/task', taskRoute);
 
 // Não excluir, exportação necessária para os testes!!!
 module.exports = app;
