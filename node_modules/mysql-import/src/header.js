c39705c42b21481fb49cde1b2ba3fe0f42e60e6a
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-23 11:18:03 -0300

    feat: docker-compose e banco criado para o esquenta

---
+++ node_modules/mysql-import/src/header.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const mysql = require('mysql');
+const fs = require('fs');
+const path = require("path");
+const stream = require('stream');
---
+++ test/restoreDb.js
@@ -0,0 +1,22 @@
+const Importer = require("mysql-import");
+require("dotenv").config();
+
+const restoreDb = async () => {
+    const { MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST } = process.env;
+
+    const importer = new Importer({
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        host: MYSQL_HOST,
+      });
+
+      await importer.import("./EsquentaMonitoria.sql");
+
+      await importer.disconnect();
+};
+
+module.exports = restoreDb;
+
+if (!module.parent) {
+    restoreDb();
+}
