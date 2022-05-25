f038b2591f6eed8c17a7232437e707d3d7a53e8e
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-05-25 15:17:10 -0300

    feat: model createTalker criada

--- Esquenta-Store-Manager/src/models/talkersModel.js
+++ Esquenta-Store-Manager/src/models/talkersModel.js
@@ -20,7 +20,20 @@ const getTalkerById = async ({ id }) => {
   return response;
 };
 
+const createTalker = async ({ id, name, age, email }) => {
+  const query = `
+    INSERT INTO talkers (name_talker, age_talker, email_talker)
+    VALUES (?, ?, ?)
+    WHERE id_talker = ?
+  `;
+
+  const [response] = await connection.execute(query, [name, age, email, id]);
+
+  return response;
+};
+
 module.exports = {
   getAllTalkers,
   getTalkerById,
+  createTalker,
 }
