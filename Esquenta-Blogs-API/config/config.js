9267776ce5b2b64b60554711328d9a1ee47c29f2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-06 16:40:00 -0300

    fix(config): correct db password and add customize db port option

--- Esquenta-Blogs-API/config/config.js
+++ Esquenta-Blogs-API/config/config.js
@@ -3,23 +3,26 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   },
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   },
 };
