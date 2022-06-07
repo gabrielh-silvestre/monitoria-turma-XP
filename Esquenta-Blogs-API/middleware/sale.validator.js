211c2d6b0c8c2af899e4bdb94f52c9822e8394da
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-07 19:28:43 -0300

    refactor(middleware/sale): change the way it validates products structure

--- Esquenta-Blogs-API/middleware/sale.validator.js
+++ Esquenta-Blogs-API/middleware/sale.validator.js
@@ -1,20 +1,30 @@
 const joi = require('joi');
 const { BadRequestError, UnprocessableEntityError } = require('restify-errors');
 
-const saleCreationSchema = joi.array().items(
-  joi.object({
-    productId: joi.number().required(),
-    quantity: joi.number().positive().min(1).required(),
-  })
-);
+const saleCreationSchema = joi.object({
+  productId: joi.number().required(),
+  quantity: joi.number().positive().min(1).required(),
+});
 
 const createValidation = (req, _res, next) => {
-  const { error } = saleCreationSchema.validate(req.body);
+  if (req.body.length === 0) {
+    throw new BadRequestError('"products" is required');
+  }
 
-  if (error) {
-    throw error.details[0].message.includes('required')
-      ? new BadRequestError(error.details[0].message)
-      : new UnprocessableEntityError(error.details[0].message);
+  const errors = req.body.reduce((acc, product) => {
+    const { error } = saleCreationSchema.validate(product);
+
+    if (error) {
+      return [...acc, error];
+    }
+
+    return acc;
+  }, []);
+
+  if (errors.length > 0) {
+    throw errors[0].details[0].message.includes('required')
+      ? new BadRequestError(errors[0].details[0].message)
+      : new UnprocessableEntityError(errors[0].details[0].message);
   }
 
   next();
