deb92ebb374772d5fec2dbf7ea401053fd3b5ad6
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-07 17:40:38 -0300

    feat(routes): implement validation middleware on routes
    - apply express.json before app routes

--- Esquenta-Blogs-API/index.js
+++ Esquenta-Blogs-API/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const rescue = require('express-rescue');
 
 const router = require('./routes');
+const ErrorHandler = require('./middleware/error.handler');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,7 +12,10 @@ app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
+app.use(express.json());
 app.use(router);
+
+app.use(rescue.from(Error, ErrorHandler));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- Esquenta-Blogs-API/routes/product.router.js
+++ Esquenta-Blogs-API/routes/product.router.js
@@ -2,12 +2,24 @@ const { Router } = require('express');
 const rescue = require('express-rescue');
 
 const ProductController = require('../controller/product.controller');
+const ProductValidator = require('../middleware/product.validator');
 
 const router = Router();
 
 router.get('/', rescue(ProductController.findAll));
-router.post('/', rescue(ProductController.create));
-router.put('/:id', rescue(ProductController.update));
+
+router.post(
+  '/',
+  rescue(ProductValidator.createValidation),
+  rescue(ProductController.create)
+);
+
+router.put(
+  '/:id',
+  rescue(ProductValidator.createValidation),
+  rescue(ProductController.update)
+);
+
 router.delete('/:id', rescue(ProductController.remove));
 
 module.exports = router;
