d74bc2248ecf24c1dc94f1771ea9fa00849734f5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-06-10 08:41:44 -0300

    feat(model): implement resolution

--- Esquenta-Blogs-API/model/product.model.js
+++ Esquenta-Blogs-API/model/product.model.js
@@ -1,4 +1,4 @@
-const { Product } = require('../database/models')
+const { Product } = require('../database/models');
 
 const findAll = async () => {
   const product = await Product.findAll();
@@ -6,23 +6,38 @@ const findAll = async () => {
 };
 
 const findById = async ({ id }) => {
-  // retorna um produto ou null caso não exista
+  const product = await Product.findByPk(id);
+
+  return product;
 };
 
 const findByName = async ({ name }) => {
-  // retorna um produto ou null caso não exista
+  const product = await Product.findOne({ where: { name } });
+
+  return product ? product[0] : null;
 };
 
 const create = async ({ name, quantity }) => {
-  // retorna o produto criado
+  const product = await Product.create({ name, quantity });
+  return product;
 };
 
 const update = async ({ id, name, quantity }) => {
-  // retorna o produto atualizado
+  const product = await Product.findByPk(id);
+
+  product.name = name;
+  product.quantity = quantity;
+  await product.save();
+
+  return product;
 };
 
 const remove = async ({ id }) => {
-  // não retorna nada
+  const product = await Product.findByPk(id);
+
+  await product.destroy();
+
+  return true;
 };
 
 module.exports = { findAll, findById, findByName, create, update, remove };
--- Esquenta-Blogs-API/model/sale.model.js
+++ Esquenta-Blogs-API/model/sale.model.js
@@ -1,28 +1,46 @@
-const { Sale, Product } = require('../database/models')
+const { Sale, Product, SaleProduct, sequelize } = require('../database/models');
 
 const findAll = async () => {
   const sale = await Sale.findAll({
     include: [
       {
         model: Product,
-        as: 'product',
+        as: 'products',
         through: { attributes: [] },
       },
     ],
   });
-
   return sale;
 };
 
 const create = async (products) => {
-  // retorna a venda criada
-  /*
-  formato da venda:
-  {
-    id,
-    date,
+  const createTransaction = await sequelize.transaction();
+
+  try {
+    const sale = await Sale.create();
+
+    const attProducts = products.map(async (p) => {
+      const product = await Product.findByPk(p.productId);
+
+      product.quantity -= p.quantity;
+      await product.save();
+
+      await SaleProduct.create({
+        saleId: sale.id,
+        productId: p.productId,
+        quantity: p.quantity,
+      });
+
+      return product;
+    });
+
+    await Promise.all(attProducts);
+
+    return sale;
+  } catch (error) {
+    await createTransaction.rollback();
+    return null;
   }
-  */
 };
 
 module.exports = { findAll, create };
